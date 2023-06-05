8968f2f261af07e5397afd81136224ef2f8bb961
Talvane Ramos <69480959+talvaneramos@users.noreply.github.com>
2023-06-05 10:48:03 -0300

    SqlQuery1.sql
    Create das tabelas Cliente e Dependente

---
+++ SQLQuery1.sql
@@ -0,0 +1,27 @@
+﻿
+//Criação da tabela Cliente
+create table Cliente(
+
+IdCliente        integer              identity(1,1),
+Nome             nvarchar(150)        not null, 
+Email            nvarchar(150)        not null unique,
+Cpf              nvarchar(15)         not null unique,
+primary key(IdCliente))
+
+
+//Criação da Tabela Dependente
+create table Dependente(
+
+IdDependente     integer             identity(1,1),
+Nome             nvarchar(150)       not null, 
+DataNascimento   date                not null, 
+IdCliente        integer             not null,
+primary key (IdDependente),
+foreign key (IdCliente) references Cliente(IdCliente))
+
+
+
+select * from Dependente d
+inner join Cliente c
+on d.IdCliente = c.IdCliente
+
